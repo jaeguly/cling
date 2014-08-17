48a7204b58eb0ea7b5f1ceee5abf207873945c03
Andrew Jang <jaeguly@gmail.com>
2014-08-17 17:49:32 +0900

    added hard-coded container and items of cds service as mockup.

--- contrib/mediaserver/lib/src/main/java/org/fourthline/cling/mediaserver/android/AndroidContentDirectoryService.java
+++ contrib/mediaserver/lib/src/main/java/org/fourthline/cling/mediaserver/android/AndroidContentDirectoryService.java
@@ -9,8 +9,16 @@ import org.fourthline.cling.support.model.BrowseFlag;
 import org.fourthline.cling.support.model.BrowseResult;
 import org.fourthline.cling.support.model.DIDLContent;
 import org.fourthline.cling.support.model.DIDLObject;
+import org.fourthline.cling.support.model.PersonWithRole;
+import org.fourthline.cling.support.model.Res;
 import org.fourthline.cling.support.model.SortCriterion;
 import org.fourthline.cling.support.model.container.StorageFolder;
+import org.fourthline.cling.support.model.item.MusicTrack;
+import org.fourthline.cling.support.model.item.Photo;
+import org.seamless.util.MimeType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AndroidContentDirectoryService extends AbstractContentDirectoryService {
 
@@ -20,6 +28,10 @@ public class AndroidContentDirectoryService extends AbstractContentDirectoryServ
                                long firstResult, long maxResults, SortCriterion[] orderBy)
             throws ContentDirectoryException {
         DIDLContent didlContent = new DIDLContent();
+
+        Log.i(TAG, "browse(objectID:" + objectID + ", browseFlag:" + browseFlag
+                + ", filter:" + filter + ", firstResult:" + firstResult
+                + ", maxResults:" + maxResults + ", orderby:" + orderBy);
 
         int resultCount = 0;
 
@@ -35,9 +47,7 @@ public class AndroidContentDirectoryService extends AbstractContentDirectoryServ
                 resultCount = 1;
             }
 
-        }
-
-        if ("0".equalsIgnoreCase(objectID)) {
+        } else if ("0".equalsIgnoreCase(objectID)) {
 
             // build a music container
             StorageFolder musicContainer = new StorageFolder("1", "0", "Music", null, 2, null);
@@ -52,6 +62,26 @@ public class AndroidContentDirectoryService extends AbstractContentDirectoryServ
             didlContent.addContainer(musicContainer);
             didlContent.addContainer(photoContainer);
             resultCount = 2;
+
+        } else if ("1".equalsIgnoreCase(objectID)) {
+
+            List<MusicTrack> tracks = createMusicTracks("1");
+
+            for (MusicTrack track : tracks) {
+                didlContent.addItem(track);
+            }
+
+            resultCount = tracks.size();
+
+        } else if ("2".equalsIgnoreCase(objectID)) {
+
+            List<Photo> photos = createPhotos("2");
+
+            for (Photo photo : photos) {
+                didlContent.addItem(photo);
+            }
+
+            resultCount = photos.size();
         }
 
         try {
@@ -63,11 +93,59 @@ public class AndroidContentDirectoryService extends AbstractContentDirectoryServ
         }
     }
 
+    private List<MusicTrack> createMusicTracks(String parentId) {
+        List<MusicTrack> result = new ArrayList<MusicTrack>();
+
+        String album = "Music";
+        String creator = "Unknown";
+        PersonWithRole artist = new PersonWithRole(creator, "");
+        MimeType mimeType = new MimeType("audio", "mpeg");
+        MusicTrack musicTrack = new MusicTrack(
+                "101", parentId, "Track 1", creator, album, artist,
+                new Res(mimeType, 123456l, "00:02:33", 8192L,
+                        "http://api.jamendo.com/get2/stream/track/redirect/?id=310355&streamencoding=mp31"));
+        musicTrack.setRestricted(true);
+        result.add(musicTrack);
+
+        musicTrack = new MusicTrack(
+                "102", parentId, "Track 2", creator, album, artist,
+                new Res(mimeType, 123456l, "00:02:01", 8192L,
+                        "http://api.jamendo.com/get2/stream/track/redirect/?id=310370&streamencoding=mp31"));
+        musicTrack.setRestricted(true);
+        result.add(musicTrack);
+
+        return result;
+    }
+
+    private List<Photo> createPhotos(String parentId) {
+        List<Photo> result = new ArrayList<Photo>();
+
+        String album = null;
+        String creator = null;
+        MimeType mimeType = new MimeType("image", "jpg");
+
+        String url = "https://a-v2.sndcdn.com/assets/images/front/zeds_dead@2x-0107f5.jpg";
+        Photo photo = new Photo("201", parentId, url, creator, album,
+                new Res(mimeType, 123456L, url));
+        photo.setRestricted(true);
+        photo.setClazz(new DIDLObject.Class("object.item.imageItem"));
+        result.add(photo);
+
+        url = "https://a-v2.sndcdn.com/assets/images/front/little_simz@2x-30fac7.jpg";
+        photo = new Photo("202", parentId, url, creator, album, new Res(
+                mimeType, 123456L, url));
+        photo.setRestricted(true);
+        photo.setClazz(new DIDLObject.Class("object.item.imageItem"));
+        result.add(photo);
+
+        return result;
+    }
 
     @Override
     public BrowseResult search(String containerId, String searchCriteria, String filter, long firstResult,
                                long maxResults, SortCriterion[] orderBy) throws ContentDirectoryException {
-        // You can override this method to implement searching!
+        Log.i("MyContentDirectoryService", "search(): containerId = " + containerId + ", searchCriteria = "
+                + searchCriteria);
 
         return super.search(containerId, searchCriteria, filter, firstResult, maxResults, orderBy);
     }
